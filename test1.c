c95905e8e3c51c97e3643951f353de96784fc576
hombremuerto <sc1enc3.and.you@gmail.com>
2019-09-15 12:57:15 +0300

    sort

--- test1.c
+++ test1.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-	printf("hello world");
+	time_t t;
+	srand(time(&t));
+	int num[10];
+	int outer, inner, ctr, buf;
+	for (ctr = 0; ctr < 10; ctr++) {
+		num[ctr] = (rand() % 99) + 1;
+	}
+	for (ctr = 0; ctr < 10; ctr++) {
+		printf("   %d\n", num[ctr]);
+	}
+	
+	for (outer = 0; outer < 9; outer++) {
+		for (inner = outer; inner < 10; inner++) {
+			if (num[inner] < num[outer]) {
+				buf = num[inner];
+				num[inner] = num[outer];
+				num[outer] = buf;
+			}
+		}
+		
+	}
+	for (ctr = 0; ctr < 10; ctr++) {		
+		printf(" %d\n", num[ctr]);
+		}
+	return 0;
+	
+	
 }
